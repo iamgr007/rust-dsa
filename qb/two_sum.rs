8ea24e3820c034ceb0e16c386e017ba24e3a0772
qb <83409253+kalyan-raparthi@users.noreply.github.com>
2025-04-16 09:37:29 -0400

    Update two_sum.rs

--- qb/two_sum.rs
+++ qb/two_sum.rs
@@ -1,4 +1,14 @@
 // two sum problem
 
 fn main() {
+}fn main() {
+  let arr = [2, 7, 11, 15];
+    
+    for i in 0..arr.len() {
+        for j in 0..arr.len()  {
+            if arr[i] + arr[j] == 9 {
+                println!("{}{}", i, j);
+            }
+        }
+    }
 }
