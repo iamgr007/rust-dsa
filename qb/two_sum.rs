98b6f19d270bdae53a70c964e22b1b7b1583cef0
qb <83409253+kalyan-raparthi@users.noreply.github.com>
2025-04-17 22:58:25 +0530

    Update two_sum.rs

--- qb/two_sum.rs
+++ qb/two_sum.rs
@@ -1,11 +1,11 @@
 // two sum problem
 
 fn main() {
-}fn main() {
-  let arr = [2, 7, 11, 15];
-    
-    for i in 0..arr.len() {
-        for j in 0..arr.len()  {
+    let arr = [2, 7, 11, 15];
+    let N = arr.len();
+  
+    for i in 0..N {
+        for j in 0..N  {
             if arr[i] + arr[j] == 9 {
                 println!("{}{}", i, j);
             }
